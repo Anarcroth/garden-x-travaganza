e772f5c6636eb078fbde4650675d4a5d08b2577f
Martin Nestorov <mdinestorov@gmail.com>
2018-11-05 23:31:58 +0200

    Fixed some sql statements

--- sql/statements.sql
+++ sql/statements.sql
@@ -4,9 +4,9 @@ USE garden_x_travaganza;
 CREATE TABLE users (username VARCHAR(50), password VARCHAR(100));
 
 -- Populate a database
-INSERT INTO users (username,password) VALUES("Olivia","1234567890");
+INSERT INTO users (username,password) VALUES("Olivia","1234567890"); -- This is only a demo data input
 
 -- Create a user with privileges to a database
 CREATE user 'martin'@'localhost' IDENTIFIED BY 'me4kaikop4e';
-GRANT ALL PRIVILEGES ON garden_x_travaganza.* TO 'martin'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'martin'@'localhost';
 FLUSH PRIVILEGES;

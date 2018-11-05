10552cbd6f8ac5deb4575be0e13bceddc724d647
Martin Nestorov <mdinestorov@gmail.com>
2018-11-05 23:31:47 +0200

    Made a connection between the local db and the login page

--- src/login.php
+++ src/login.php
@@ -1,33 +1,34 @@
 <?php
 session_start();
 
-// TODO add validation for the input
 $userName = $_POST['userName'];
 $_SESSION['userName']= $username;
 
 $userPass = $_POST['passwd'];
 $_SESSION['passwd'] = $userPass;
 
-
-// Add your database name (i.e. username), username and password
-$servername = "localhost";
-$dbname = "mdn";
+$servername = "127.0.0.1";
+$dbname = "garden_x_travaganza";
 $dsn = "mysql:host=$servername;dbname=$dbname";
-$user = "mdn";
-$pass = "1234";
+$user = "martin";
+$pass = "me4kaikop4e";
 
 try {
     $conn = new PDO($dsn, $user, $pass);
 
-    $sql = "";
-
-    $result = $conn->exec($sql);
-
-    $sql = 'SELECT * FROM customers';
+    $sql = 'SELECT * FROM users';
 
     $result = $conn->query($sql);
 
-    htmlTable($result);
+    $authenticated = false;
+    foreach ($result as $u) {
+        if ($u['username'] == $userName) {
+            $authenticated = true;
+        }
+    }
+
+    echo $authenticated;
+
 }   catch(PDOException $e) {
     echo $sql . "<br>" . $e->getMessage();
 }

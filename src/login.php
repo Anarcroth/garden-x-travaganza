031d17fca318c3424e364c0feacc9b4b8ff2ba07
Martin Nestorov <mdinestorov@gmail.com>
2018-11-06 21:02:05 +0200

    Added registration and login to the DB

--- src/login.php
+++ src/login.php
@@ -1,35 +1,44 @@
 <?php
 session_start();
 
-$userName = $_POST['userName'];
-$_SESSION['userName']= $username;
+$userName;
+$userPass;
+if (isset($_POST['userName']) && isset($_POST['passwd'])) {
+    $userName = $_POST['userName'];
+    $_SESSION['userName']= $userName;
 
-$userPass = $_POST['passwd'];
-$_SESSION['passwd'] = $userPass;
-
-$servername = "127.0.0.1";
-$dbname = "garden_x_travaganza";
-$dsn = "mysql:host=$servername;dbname=$dbname";
-$user = "martin";
-$pass = "me4kaikop4e";
+    $userPass = $_POST['passwd'];
+    $_SESSION['passwd'] = $userPass;
+} else {
+    echo "<br> Username or Password were not set!";
+}
 
 try {
+    $servername = "127.0.0.1";
+    $dbname = "garden_x_travaganza";
+    $dsn = "mysql:host=$servername;dbname=$dbname";
+    $user = "martin";
+    $pass = "me4kaikop4e";
+
     $conn = new PDO($dsn, $user, $pass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT * FROM users';
+    $selectAllQuery = "SELECT * FROM users WHERE username='".$userName."'";
+    $allUsers = $conn->query($selectAllQuery);
+    foreach ($allUsers as $u) {
+        $dbPass = $u['password'];
+    }
 
-    $result = $conn->query($sql);
-
+    // Checks that the user password is a valid hash of the saved passwrd in the DB
     $authenticated = false;
-    foreach ($result as $u) {
-        if ($u['username'] == $userName) {
-            $authenticated = true;
-        }
+    if(password_verify($userPass, $dbPass)) {
+        $authenticated = true;
     }
 
     echo $authenticated;
 
 }   catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+    echo $selectAllQuery . "<br>" . $e->getMessage();
 }
 ?>
---
+++ src/register.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+$newUser;
+$hashed_password;
+if (isset($_POST['newUserName']) && isset($_POST['newPasswd'])) {
+    $newUser = $_POST['newUserName'];
+    //    $_SESSION['newUserName']= $newUser;
+
+    $hashed_password = password_hash($_POST['newPasswd'], PASSWORD_DEFAULT);
+    //    $_SESSION['newPasswd'] = $hashed_password;
+} else {
+    echo "<br> Username or Password were not set!";
+}
+
+try {
+    $servername = "127.0.0.1";
+    $dbname = "garden_x_travaganza";
+    $dsn = "mysql:host=$servername;dbname=$dbname";
+    $user = "martin";
+    $pass = "me4kaikop4e";
+
+    $conn = new PDO($dsn, $user, $pass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $selectAllQuery = "SELECT * FROM users WHERE username='".$newUser."'";
+    $allUsers = $conn->query($selectAllQuery);
+    $userNames = [];
+    foreach ($allUsers as $u) {
+        $userNames[] = $u['username'];
+    }
+
+    // Server side check that the new username is unique
+    if (in_array($newUser, $users)) {
+        echo "This username is already taken!";
+    } else {
+        $sql = "INSERT INTO users (username,password) VALUES(\"".$newUser."\",\"".$hashed_password."\");";
+        $conn->exec($sql);
+        echo "Registered!";
+    }
+
+}   catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+?>

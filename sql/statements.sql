ed4b7e694c6bc728eb6d09a37980696713b4ab91
Martin Nestorov <mdinestorov@gmail.com>
2018-11-06 22:00:57 +0200

    Added deletion of table entry sql

--- sql/statements.sql
+++ sql/statements.sql
@@ -6,6 +6,9 @@ CREATE TABLE users (username VARCHAR(50), password VARCHAR(100));
 -- Populate a database
 INSERT INTO users (username,password) VALUES("Olivia","1234567890"); -- This is only a demo data input
 
+-- Remove table entries
+ DELETE FROM users WHERE username='XXXX';
+
 -- Create a user with privileges to a database
 CREATE user 'martin'@'localhost' IDENTIFIED BY 'me4kaikop4e';
 GRANT ALL PRIVILEGES ON *.* TO 'martin'@'localhost';

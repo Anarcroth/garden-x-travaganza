108d6971deaa1b872c561d72219b229f3f1e9e3a
Martin Nestorov <mdinestorov@gmail.com>
2018-11-09 20:17:41 +0200

    Added cart for checkout

--- src/showCart.php
+++ src/showCart.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-// Check if user wants to end session and empty cart
-if (isset($_GET['checkout'])) { 
+
+if (isset($_GET['checkout'])) {
 	session_unset();
-	session_destroy(); 
-	// header('location: ' . $_SERVER['PHP_SELF']); 
+	session_destroy();
+	// header('location: ' . $_SERVER['PHP_SELF']);
 	exit();
 }
 ?>
@@ -13,26 +13,54 @@ if (isset($_GET['checkout'])) {
 <body>
 
 <table border="1">
-
-<thead> <tr> <th>Items in Shopping Cart</th> <th>Price</th> </tr>
+             <thead> <tr> <th>Items in Shopping Cart</th> <th>Price</th> </tr>
 
 <?php
-	$items = array('Item 1','Item 2','Item 3','Item 4');
+$catalogue = [];
+$items = [];
+$ids = [];
+$prices = [];
+$descriptions = [];
+$imgPaths = [];
+$codes = [];
 
-	$prices = array( 24.95, 17.85, 19.99, 34.95 );
+try {
+    $servername = "127.0.0.1";
+    $dbname = "garden_x_travaganza";
+    $dsn = "mysql:host=$servername;dbname=$dbname";
+    $user = "martin";
+    $pass = "me4kaikop4e";
 
-	$totalItems = count($_SESSION['cart']);
-	$totalCost = 0.00;
-	
+    $conn = new PDO($dsn, $user, $pass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $selectAllItems = "SELECT * FROM catalogue";
+    $catalogue = $conn->query($selectAllItems);
+
+} catch (PDOException $e) {
+    echo "<br>" . $e->getMessage();
+}
+foreach ($catalogue as $i) {
+    $items[] = $i['item'];
+    $ids[] = $i['id'];
+    $prices[] = $i['price'];
+    $descriptions[] = $i['description'];
+    $imgPaths[] = $i['imgpath'];
+}
+
+$totalItems = count($_SESSION['cart']);
+$totalCost = 0.00;
+
 // Loop over items in shopping cart
 for ($i = 0; $i < $totalItems; $i++) {
 	echo '<tr>';
 
-	echo '<td>' . $items[$_SESSION['cart'][$i]] . '</td>'; 
-	echo '<td>' . $prices[$_SESSION['cart'][$i]] . '</td>'; 
-	
+	echo '<td>' . $items[$_SESSION['cart'][$i]] . '</td>';
+	echo '<td>' . $prices[$_SESSION['cart'][$i]] . '</td>';
+
 	echo '</tr>';
-	
+
 	$totalCost = $totalCost + $prices[$_SESSION['cart'][$i]];
 }
 echo '</table>';
@@ -43,6 +71,5 @@ echo 'Total cost of items bought= ' . $totalCost;
 ?>
 
 <p><a href="showCatalogue.php">Continue Shopping</a> or <a href="<?php echo $_SERVER['PHP_SELF']; ?>?checkout='true'">Check Out</a></p>
-</body>
-</html>
-
+                                                                                                                                </body>
+                                                                                                                                      </html>

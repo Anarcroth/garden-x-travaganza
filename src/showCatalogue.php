eb63f9e4e7a6c714e225ae25f0a5380ef89994a3
Martin Nestorov <mdinestorov@gmail.com>
2018-11-09 19:55:50 +0200

    Added catalogue listings

--- src/showCatalogue.php
+++ src/showCatalogue.php
@@ -1,61 +1,80 @@
 <?php
 session_start();
 
-	// Set items for sale and their prices
-	$items = array('Item 1','Item 2','Item 3','Item 4');
+$catalogue = [];
+$items = [];
+$ids = [];
+$prices = [];
+$descriptions = [];
+$imgPaths = [];
+$codes = [];
 
-	$prices = array( 24.95, 17.85, 19.99, 34.95 );
-	
-// Check if shopping cart exists or not as SESSION array
-if (!isset($_SESSION['cart'])) 
-	{
-		// No, so create empty cart as a PHP SESSION array
-		$_SESSION['cart'] = array();
-	}
+try {
+    // TODO set this object into a session or a separate file that would be called when needed
+    $servername = "127.0.0.1";
+    $dbname = "garden_x_travaganza";
+    $dsn = "mysql:host=$servername;dbname=$dbname";
+    $user = "martin";
+    $pass = "me4kaikop4e";
 
-// Check if an item to buy has been clicked
-if (isset($_GET['buy']))
-	{
-	// Add item to the end of the $_SESSION['cart'] array 
-	$_SESSION['cart'][] = $_GET['buy']; 
-	header('location: ' . $_SERVER['PHP_SELF']); 
+    $conn = new PDO($dsn, $user, $pass);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $selectAllItems = "SELECT * FROM catalogue";
+    $catalogue = $conn->query($selectAllItems);
+
+} catch (PDOException $e) {
+    echo "<br>" . $e->getMessage();
+}
+
+foreach ($catalogue as $i) {
+    $items[] = $i['item'];
+    $ids[] = $i['id'];
+    $prices[] = $i['price'];
+    $descriptions[] = $i['description'];
+    $imgPaths[] = $i['imgpath'];
+}
+
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
+}
+
+if (isset($_GET['buy'])) {
+	$_SESSION['cart'][] = $_GET['buy'];
+	header('location: ' . $_SERVER['PHP_SELF']);
 	exit();
-	}
+}
 ?>
 
 <html>
 <body>
-	
-	<p>Your shopping cart contains <?php echo count($_SESSION['cart']); ?> items.</p>
-	
-	<p> Click on an item to add the item to your shopping cart</p>
+
+<p>Your shopping cart contains <?php echo count($_SESSION['cart']); ?> items.</p>
+
+<p> Click on an item to add the item to your shopping cart</p>
 
 <?php
-	// Display catalogue items for sale in a table
-	echo '<table border="1">';
-	for ($i = 0; $i < count($items); $i++) {
-		// Generate table rows
-		echo '<tr>';
+echo '<table border="1">';
+for ($i = 0; $i < count($items); $i++) {
+    echo '<tr>';
+    echo '<td>' . $items[$i] . '</td>';
+    echo '<td>$' . $prices[$i] . '</td>';
+    echo '<td>' . $descriptions[$i] . '</td>';
+    echo '<td><img src="' . $imgPaths[$i] . '" width="100" height="100"></img></td>';
 
-		echo '<td>' . $items[$i] . '</td>';
-		echo '<td>$' . $prices[$i] . '</td>';
+    // Add a buy link for each item on sale
+    echo '<td><a href="' . $_SERVER['PHP_SELF'] .'?buy=' . $i . '">Buy</a></td>';
 
-		// Add a buy link for each item on sale
-		echo '<td><a href="' . $_SERVER['PHP_SELF'] .'?buy=' . $i . '">Buy</a></td>';
+    echo '</tr>';
+}
+echo '</table>';
 
-		echo '</tr>';
-	}
-	echo '</table>';
-	
-	echo '<br />';
-	
-	echo "Click here to ";
+echo '<br />';
+
+echo "Click here to ";
 ?>
-
-	<a href='showCart.php'>see items in cart</a>
+<a href='showCart.php'>see items in cart</a>
 
 </body>
 </html>
-
-
-

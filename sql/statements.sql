25007882fc5c6b08cb5125554277454889f52069
Martin Nestorov <mdinestorov@gmail.com>
2018-11-09 19:56:06 +0200

    Updated the value and types of the catalogue

--- sql/statements.sql
+++ sql/statements.sql
@@ -22,12 +22,12 @@ CREATE TABLE promo (code VARCHAR(50), item TINYTEXT);
 
 INSERT INTO users (username, password) VALUES("Olivia","1234567890"); -- This is only a demo. Real users should have their passwords encrypted and NOT stored in plain text.
 
-INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Hedge Trimmer", 421234, 149.00, "Kobalt 80-volt cordless hedge trimmer provides the power you need with up to 70 minutes runtime on a fully charged 2.0 Ah battery. Complete your project faster with dual action laser cut blades at 3600 strokes per minute. Cut through thick shrubs with a 26-in blade and 3/4-in cut capacity.", "./assets/hedge_trimmer.jpg");
-INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Root Assassin Shovel", 468713, 50.00, "48 In ROOT ASSASSIN shovel saw that is the original and best award winning shovel and saw Combo garden tool. Patented all purpose garden shovel and saw easily slices through roots. 16 beveled and serrated steel teeth on each side and cuts while digging both in and out.", "./assets/shovel.jpg");
-INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Bypass Hand Pruner", 541896, 14.98, "A pruner with soft ComfortGEL grips for less hand fatigue and a great feel. The handles, chassis and hook are a one-piece, full-steel design built for durability. The handles have a full steel core versus other tools which have a steel blade connected to the top of a handle.", "./assets/pruner.jpg");
-INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Bypass Lopper", 528965, 23.48, "Power-lever mechanism increases leverage to make cutting 2 times easier than single-pivot loppers. Sharp, precision-ground blade edge. Corrosion-resistant, non-stick blade coating reduces friction to make cutting easier.", "./assets/lopper.jpg");
-INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Sledge Hammer", 456851, 34.98, "10 lb steel sledge hammer with 34-in fiberglass handle. Demolition side perfect for all those difficult tasks around a work site. 10-lb head is ideal for concrete and construction projects.", "./assets/sledge_hammer.jpg");
-INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Steel Camp Axe", 965810, 34.98, "Great for use on camping trips. 1-piece forged metal head and handle for strength. 3-1/4 in. tempered cutting edge for easy chopping.", "./assets/camp_axe.jpg");
+INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Hedge Trimmer", 421234, 149.00, "Kobalt 80-volt cordless hedge trimmer provides the power you need with up to 70 minutes runtime on a fully charged 2.0 Ah battery. Complete your project faster with dual action laser cut blades at 3600 strokes per minute. Cut through thick shrubs with a 26-in blade and 3/4-in cut capacity.", "/hedge_trimmer.jpg");
+INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Root Assassin Shovel", 468713, 50.00, "48 In ROOT ASSASSIN shovel saw that is the original and best award winning shovel and saw Combo garden tool. Patented all purpose garden shovel and saw easily slices through roots. 16 beveled and serrated steel teeth on each side and cuts while digging both in and out.", "/shovel.jpg");
+INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Bypass Hand Pruner", 541896, 14.98, "A pruner with soft ComfortGEL grips for less hand fatigue and a great feel. The handles, chassis and hook are a one-piece, full-steel design built for durability. The handles have a full steel core versus other tools which have a steel blade connected to the top of a handle.", "/pruner.jpg");
+INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Bypass Lopper", 528965, 23.48, "Power-lever mechanism increases leverage to make cutting 2 times easier than single-pivot loppers. Sharp, precision-ground blade edge. Corrosion-resistant, non-stick blade coating reduces friction to make cutting easier.", "/lopper.jpg");
+INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Sledge Hammer", 456851, 34.98, "10 lb steel sledge hammer with 34-in fiberglass handle. Demolition side perfect for all those difficult tasks around a work site. 10-lb head is ideal for concrete and construction projects.", "/sledge_hammer.jpg");
+INSERT INTO catalogue (item, id, price, description, imgpath) VALUES("Steel Camp Axe", 965810, 34.98, "Great for use on camping trips. 1-piece forged metal head and handle for strength. 3-1/4 in. tempered cutting edge for easy chopping.", "/camp_axe.jpg");
 
 INSERT INTO promo (code, item) VALUES("6daee431-5882-416b-b60a","Sledge Hammer");
 INSERT INTO promo (code, item) VALUES("3b632813-6b36-4563-bf16","Hedge Trimmer");
@@ -38,11 +38,17 @@ INSERT INTO promo (code, item) VALUES("6a2b4397-c840-405f-ab8e","Bypass Lopper")
 
 -- Plumbing commands --
 
+-- Just get everything.
+SELECT * FROM tablename;
+
 -- Remove table entries.
 DELETE FROM users WHERE username='XXXX';
 
 -- Update table column data types.
-ALTER TABLE users ALTER COLUMN username TINYTEXT;
+ALTER TABLE users MODIFY username TINYTEXT; -- This is just an example on how to modify a column data type.
 
 -- Show the details of a table.
 SHOW FIELDS FROM table_name;
+
+-- Update a specific column value of a table;
+UPDATE catalogue SET imgpath='$HOME/assets/hedge_trimmer.jpg' WHERE item='Hedge Trimmer';

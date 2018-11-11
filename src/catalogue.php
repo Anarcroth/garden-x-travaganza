4a7570b656f8efbe944cca40b6f64a9a55e2f7bd
Martin Nestorov <mdinestorov@gmail.com>
2018-11-11 22:30:05 +0200

    Refactored the catalogue object class to have shorter exception handling

--- src/catalogue.php
+++ src/catalogue.php
@@ -6,45 +6,33 @@ class catalogue {
     }
 
     function getAll() {
-        try {
-            $sql = "SELECT * FROM catalogue";
-            return $this->exec($sql);
-        } catch (PDOException $pdoe) {
-            echo "<br>Could not fetch all items<br>".$pdoe->getMessage();
-        }
+        $sql = "SELECT * FROM catalogue";
+        return $this->exec($sql);
     }
 
     function getItemByName($name) {
-        try {
-            $sql = "SELECT * FROM catalogue WHERE item='".$name."'";
-            return $this->exec($sql);
-        } catch (PDOException $pdoe) {
-            echo "<br>Could not fetch item ".$name."<br>".$pdoe->getMessage();
-        }
+        $sql = "SELECT * FROM catalogue WHERE item='".$name."'";
+        return $this->exec($sql);
     }
 
     function getItemByCode($id) {
-        try {
-            $sql = "SELECT * FROM catalogue WHERE id='".$id."'";
-            return $this->exec($sql);
-        } catch (PDOException $pdoe) {
-            echo "<br>Could not fetch item ".$id."<br>".$pdoe->getMessage();
-        }
+        $sql = "SELECT * FROM catalogue WHERE id='".$id."'";
+        return $this->exec($sql);
     }
 
     function getSetOf($setOfItems) {
-        try {
-            $sql = "SELECT * FROM catalogue WHERE item IN ('".implode("','", $setOfItems)."')";
-            return $this->exec($sql);
-        } catch (PDOException $pdoe) {
-            echo "<br>Could not fetch item ".$id."<br>".$pdoe->getMessage();
-        }
+        $sql = "SELECT * FROM catalogue WHERE item IN ('".implode("','", $setOfItems)."')";
+        return $this->exec($sql);
     }
 
     function exec($sql) {
-        $stm = $this->db->prepare($sql);
-        $stm->execute();
-        return $stm->fetchAll();
+        try {
+            $stm = $this->db->prepare($sql);
+            $stm->execute();
+            return $stm->fetchAll();
+        } catch (PDOException $pdoe) {
+            echo "<br>Could not fetch item ".$id."<br>".$pdoe->getMessage();
+        }
     }
 }
 ?>

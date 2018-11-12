f67a7ec9cd08ca8e0fd5a5adea740b5ee67c91ad
Martin Nestorov <mdinestorov@gmail.com>
2018-11-12 23:06:40 +0200

    Added get lucky functionality, WIP

--- src/showGetLucky.php
+++ src/showGetLucky.php
@@ -9,12 +9,14 @@ $randItems = $cat->getRandomItems();
 
 // Make the received random items in a unique array
 // and also reduce the price at the same time
-$allItems = [];
-foreach ($randItems as $item) {
-    if (!in_array($item['item'], $allItems)) {
-        $randPromoAmount = mt_rand(5, 20);
-        $item['price'] -= $item['price'] * $randPromoAmount / 100;
-        $allItems[] = $item;
+if (!isset($_SESSION['allItems'])) {
+    $_SESSION['allItems'] = array();
+    foreach ($randItems as $item) {
+        if (!in_array($item['item'], $allItems)) {
+            $randPromoAmount = mt_rand(5, 20);
+            $item['price'] -= $item['price'] * $randPromoAmount / 100;
+            $_SESSION['allItems'][] = $item;
+        }
     }
 }
 
@@ -23,7 +25,7 @@ if (!isset($_SESSION['cart'])) {
 }
 
 if (isset($_GET['buy'])) {
-    $_SESSION['cart'][] = $_GET['buy'];
+    $_SESSION['luckyCart'][] = [$_GET['buy'] => $_GET['price']];
     header('location: ' . $_SERVER['PHP_SELF']);
     exit();
 }
@@ -37,16 +39,17 @@ if (isset($_GET['buy'])) {
 <p> Click on an item to add the item to your shopping cart</p>
 
 <?php
+var_dump($_SESSION['luckyCart']);
+echo $_SESSION['allItems'];
 echo '<table border="1">';
-foreach ($allItems as $i) {
+foreach ($_SESSION['allItems'] as $i) {
     echo '<tr>';
     echo '<td>' . $i['item'] . '</td>';
     echo '<td>$' . $i['price'] . '</td>';
     echo '<td>' . $i['description'] . '</td>';
     echo '<td><img src="' . $i['imgpath'] . '" width="100" height="100"></img></td>';
-
     // Add a buy link for each item on sale
-    echo '<td><a href="' . $_SERVER['PHP_SELF'] .'?buy=' . $i['item'] . '">Buy</a></td>';
+    echo '<td><a href="' . $_SERVER['PHP_SELF'] .'?buy=' . $i['item'] . '&price=' . $i['price'] . '">Buy</a></td>';
 
     echo '</tr>';
 }

c1ac60064ffb34eba5c801f83bb834a241f2be80
Martin Nestorov <mdinestorov@gmail.com>
2018-11-13 05:33:21 +0200

    Added styling to getting lucky

--- src/showGetLucky.php
+++ src/showGetLucky.php
@@ -12,7 +12,7 @@ $randItems = $cat->getRandomItems();
 if (!isset($_SESSION['allItems'])) {
     $_SESSION['allItems'] = array();
     foreach ($randItems as $item) {
-        if (!in_array($item['item'], $allItems)) {
+        if (!in_array($item['item'], $_SESSION['allItems'])) {
             $randPromoAmount = mt_rand(5, 20);
             $item['price'] -= $item['price'] * $randPromoAmount / 100;
             $_SESSION['allItems'][] = $item;
@@ -32,31 +32,39 @@ if (isset($_GET['buy'])) {
 ?>
 
 <html>
+
+    <title>Index</title>
+    <meta charset="UTF-8" name="viewport" content="initial-scale=1.0; maximum-scale=1.0; width=device-width;">
+    <link rel="stylesheet" type="text/css" href="cartstyle.php">
+
 <body>
-
-<p>Your shopping cart contains <?php echo count($_SESSION['cart']); ?> items.</p>
-
-<p> Click on an item to add the item to your shopping cart</p>
-
+<div class="table-title">
+<h3>Your shopping cart contains <?php echo count($_SESSION['cart']); ?> items.</h3>
+</div>
+<table class="table-fill">
+<thead>
+<tr>
+<th class="text-left">Items</th>
+<th class="text-left">Price</th>
+<th class="text-left">Description</th>
+<th class="text-left">Image</th>
+</tr>
+</thead>
+<tbody class="table-hover">
 <?php
-var_dump($_SESSION['luckyCart']);
-echo $_SESSION['allItems'];
-echo '<table border="1">';
 foreach ($_SESSION['allItems'] as $i) {
     echo '<tr>';
-    echo '<td>' . $i['item'] . '</td>';
-    echo '<td>$' . $i['price'] . '</td>';
-    echo '<td>' . $i['description'] . '</td>';
-    echo '<td><img src="' . $i['imgpath'] . '" width="100" height="100"></img></td>';
-    // Add a buy link for each item on sale
-    echo '<td><a href="' . $_SERVER['PHP_SELF'] .'?buy=' . $i['item'] . '&price=' . $i['price'] . '">Buy</a></td>';
-
+    echo '<td class="text-left">' . $i['item'] . '<br><br><a href="' . $_SERVER['PHP_SELF'] .'?buy=' . $i['item'] . '&price=' . $i['price'] . '">Buy</a></td>';
+    echo '<td class="text-left">$' . $i['price'] . '</td>';
+    echo '<td class="text-left">' . $i['description'] . '</td>';
+    echo '<td class="text-left"><img src="' . $i['imgpath'] . '" width="100" height="100"></img></td>';
     echo '</tr>';
 }
-echo '</table>';
+?>
+</table>
+</tbody>
 
-echo '<br />';
-
+<?php
 echo "Click here to ";
 ?>
 <a href='showCart.php'>see items in cart</a>
